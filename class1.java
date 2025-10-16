3c4a52ae24e03e6bda70931aa2f81b75071ac110
Kalagarla Akhila <akhilakalagarla@gmail.com>
2025-10-16 12:07:47 +0530

    initial commit

--- class1.java
+++ class1.java
@@ -1,5 +1,56 @@
-public class class1{
-    public static void main(String[] args) {
-        
-    }
-}
+// // Java is a platform-independent, object-oriented programming language. 
+// // Java Program to print "Hello world"
+// public class class1{
+//     public static void main(String[] args) {
+//         System.out.println("Hello world");
+//     }
+// }
+
+// // javac filename.java - To compile the code [javac class1.java]
+// // java filename - To run the code [java class1]
+
+// // DATA TYPES
+// public class class1{
+//     public static void main(String[] args) {
+//         // datatype varname = value; -----------> Syntax
+//         int a = 5; // Integer
+//         char ch = 'a'; // Character
+//         float v = 1.234f; // Floating point number
+//         byte b = 127; // Byte
+//         System.out.println(a); // Print the value of a as 5
+//         System.out.println(ch); // Print the value of ch as a
+//         System.out.println(v); // Print the value of v as 1.234
+//         System.out.println(b); // Print the value of b as 127
+//     } 
+// }
+
+// public class class1{
+//     public static void main(String[] args){
+//         int a = 5;
+//         int b = 10;
+//         a = a+b;
+//         b = a-b;
+//         a = a-b;
+//         System.out.println(a); // Print the value of a as 10
+//         System.out.println(b); // Print the value of b as 5
+//     }
+// }
+
+// // To print a star pattern
+// public class class1{
+//     public static void main(String[] args) {
+//         System.out.println("*");
+//         System.out.println("**");
+//         System.out.println("***");
+//         System.out.println("****");
+//     }
+// }
+
+// import java.util.*;
+// public class class1 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt(); 
+//         System.out.println("The integer value is "+a);
+//     }
+// }
